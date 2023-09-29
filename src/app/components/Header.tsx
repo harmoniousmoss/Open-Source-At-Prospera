9f1a8efd54df4f2604797ab4ca475296ed866c37
Syaeful Bahri <99127787+harmoniousmoss@users.noreply.github.com>
2023-09-29 14:44:22 +0700

    Change website hover

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <Link
               href="/"
-              className="text-sm font-semibold leading-6 text-gray-900"
+              className="text-sm font-semibold leading-6 text-gray-900 hover:text-cyan-500"
             >
               Website <span aria-hidden="true">&rarr;</span>
             </Link>
@@ -72,7 +72,7 @@ export default function Header() {
                 <div className="py-6">
                   <Link
                     href="https://prospera.or.id"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:text-cyan-500"
                   >
                     Website
                   </Link>
